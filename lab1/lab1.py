c733b33a0badb7387c7eb572cc8b31fe5aa45a76
Andrei Tcibin <tsibin.andr@gmail.com>
2019-10-20 14:33:44 +0300

    Добавляет первую лабу

---
+++ lab1/lab1.py
@@ -0,0 +1,62 @@
+import os
+import logging
+from datetime import datetime
+
+import numpy as np
+
+
+def init_logging(debug=False, file=None):
+    level = logging.DEBUG if debug else logging.INFO
+    handlers = [logging.StreamHandler()]
+    if file:
+        dirname = os.path.dirname(file)
+        if dirname:
+            os.makedirs(dirname, exist_ok=True)
+        handlers.append(logging.FileHandler(file))
+    logging.basicConfig(level=level, format='[%(levelname)s\t%(asctime)s] %(message)s',
+                        handlers=handlers)
+
+
+init_logging(file='logs/lab1.log.' + str(datetime.now()))
+
+step = 50
+decimals = 3
+data = np.loadtxt('data/data.txt', delimiter='\t')
+
+data_min = data.min()
+data_max = data.max()
+lower_bound = (data_min / 100).astype(int) * 100
+upper_bound = (data_max / 100).astype(int) * 100
+
+intervals = np.append(np.arange(lower_bound, upper_bound, step), upper_bound)
+occurrences = np.histogram(data, bins=intervals + 1)[0]
+frequencies = occurrences / data.size
+
+logging.info('#1')
+logging.info('intervals: ' + ','.join(intervals.astype(str)))
+logging.info('frequencies: ' + ','.join(np.around(frequencies, decimals=decimals).astype(str)))
+
+M = data.sum() / data.size
+SDNN = np.sqrt(np.power(data - M, 2).sum() / (data.size - 1))
+CV = SDNN / M * 100
+RMSSD = np.sqrt(np.power(data[:-1] - data[1:], 2).sum() / (data.size - 1))
+
+logging.info('#2')
+logging.info('M = ' + np.around(M, decimals=decimals).astype(str))
+logging.info('SDNN = ' + np.around(SDNN, decimals=decimals).astype(str))
+logging.info('CV = ' + np.around(CV, decimals=decimals).astype(str))
+logging.info('RMSSD = ' + np.around(RMSSD, decimals=decimals).astype(str))
+
+most_interval_index = np.argmax(occurrences)
+Mo = (intervals[most_interval_index] + step / 2).astype(int)
+AMo = (frequencies[most_interval_index] * 100).astype(int)
+MxDMn = (data_max - data_min).astype(int)
+
+logging.info('#3')
+logging.info('Mo = ' + Mo.astype(str))
+logging.info('AMo = ' + AMo.astype(str))
+logging.info('MxDMn = ' + MxDMn.astype(str))
+
+SI = AMo / (2 * Mo / 1000 * MxDMn / 1000)
+logging.info('#4')
+logging.info('SI = ' + np.around(SI, decimals=decimals).astype(str))
